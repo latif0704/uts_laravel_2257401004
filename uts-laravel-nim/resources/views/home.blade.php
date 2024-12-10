9dd58cb6d23b72df22c9f52ae36ea220e4cfdc8f
Hi Lenovo <latifproject0407gmail.com>
2024-12-10 14:35:48 +0700

    first commit

---
+++ uts-laravel-nim/resources/views/home.blade.php
@@ -0,0 +1,47 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Halaman Home</title>
+    
+    <style>
+        body {
+    font-family: Arial, sans-serif;
+    background-color: #f4f4f9;
+    color: #333;
+    margin: 0;
+    padding: 0;
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+    justify-content: center;
+    height: 100vh;
+}
+
+h1 {
+    color: #0066cc;
+}
+
+a {
+    text-decoration: none;
+    color: white;
+    background-color: #0066cc;
+    padding: 10px 20px;
+    border-radius: 5px;
+}
+
+a:hover {
+    background-color: #004999;
+}
+
+    </style>
+</head>
+<body>
+    <h1>Selamat Datang!</h1>
+    <p><strong>NIM:</strong>2257401038</p>
+    <p><strong>Nama:</strong>Sigit Sunanda</p>
+    <p><strong>Kelas:</strong>MI22B</p>
+    <a href="/logout">Logout</a>
+</body>
+</html>
